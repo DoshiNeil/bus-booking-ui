27badd398278ca464480a3f1d0f5c69aed0e3c5a
Neil Doshi <mail.neildoshi@gmail.com>
2024-01-11 12:35:00 +0530

    made the sidebar responsive and did basic styling

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { animated, useSpring } from 'react-spring';
 import style from './drawer.module.css';
 
@@ -6,14 +6,51 @@ type DrawerProps = {
   show: boolean;
 };
 
+// writing a simple debounce fn to re-render the sidebar on window resize
+const debounce = <F extends (...args: any[]) => any>(
+  func: F,
+  waitFor: number,
+) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  const debounced = (...args: Parameters<F>) => {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced as (...args: Parameters<F>) => ReturnType<F>;
+};
+
 export const Drawer: React.FC<DrawerProps> = ({ show }: DrawerProps) => {
+  const [windowDims, setWindowDims] = useState<{ w: number; h: number }>({
+    w: window.innerWidth,
+    h: innerHeight,
+  });
+
+  const handleResize = () =>
+    setWindowDims({
+      w: window.innerWidth,
+      h: innerHeight,
+    });
+
+  useEffect(() => {
+    window.addEventListener('resize', debounce(handleResize, 250));
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const props = useSpring({
-    left: show ? window.innerWidth - 300 : window.innerWidth,
-    position: 'absolute' as any, // temp fix as the lib is working around getting their types up to speed
-    top: 0,
-    backgroundColor: '#806290',
-    height: '100vh',
-    width: '300px',
+    left: show ? windowDims.w - 500 : windowDims.w,
+    position: 'absolute' as any,
+    backgroundColor: '#a6a6a6',
+    width: '500px',
+    height: '50rem',
+    marginTop: '-.75rem',
+    borderRadius: '8px',
   });
 
   return (
