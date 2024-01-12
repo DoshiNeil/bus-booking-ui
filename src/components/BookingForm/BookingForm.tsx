56aa459bd4a0a8929455d72b23207ab6093e278d
Neil Doshi <mail.neildoshi@gmail.com>
2024-01-12 12:17:14 +0530

    booking behaviour and validation complete

--- src/components/BookingForm/BookingForm.tsx
+++ src/components/BookingForm/BookingForm.tsx
@@ -49,62 +49,36 @@ export const BookingForm: React.FC = () => {
           default:
             break;
         }
-      } else {
-        switch (field) {
-          case PassengerDetailsField.EMAIL:
-            newDetails.passengers.push({
-              seatNumber,
-              email: value,
-              lastName: '',
-              firstName: '',
-            });
-            break;
-          case PassengerDetailsField.FIRST_NAME:
-            newDetails.passengers.push({
-              seatNumber,
-              email: '',
-              lastName: '',
-              firstName: value,
-            });
-            break;
-          case PassengerDetailsField.LAST_NAME:
-            newDetails.passengers.push({
-              seatNumber,
-              email: '',
-              lastName: value,
-              firstName: '',
-            });
-            break;
-          default:
-            break;
-        }
       }
       return newDetails;
     });
   };
 
+  const passengers = details.passengers;
+
+  // TODO
   const onSave = () => {
-    if (selectedSeats) {
-      const seats = selectedSeats.map((s) => s.seatNumber);
-      const passengersDetailsSeats = details.passengers.map(
-        (s) => s.seatNumber,
-      );
-      // final list of seats
-      const _s = passengersDetailsSeats.filter((s) => seats.includes(s));
-      //final list of passengers
-      const passengers = details.passengers.filter((s) =>
-        _s.includes(s.seatNumber),
-      );
-      if (
-        confirm(
-          `Selected seats ${JSON.stringify(
-            _s,
-          )}, shall we confirm the booking ?`,
-        )
-      ) {
-        console.table(passengers);
-        navigate('/dashboard');
-      }
+    // validate emails and check the syntax if incorrect
+    const emailPattern = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const areEmailsValid = passengers
+      .map((p) => !!p.email.match(emailPattern))
+      .reduce((prev, curr) => prev && curr);
+
+    if (!areEmailsValid) {
+      alert('Please add valid emails');
+      return;
+    }
+
+    if (
+      confirm(
+        `Selected seats ${JSON.stringify(
+          selectedSeats,
+        )}, shall we confirm the booking ?`,
+      )
+    ) {
+      // save the details to local storage
+      // reset the bus seat map
+      navigate('/dashboard');
     }
   };
 
@@ -112,45 +86,86 @@ export const BookingForm: React.FC = () => {
     let seats = [];
     if (context?.seatMap) {
       seats = context.seatMap
-        .map((m) => m.map.filter((s) => s.status === SeatStatus.SELECTED)) // filtering the selected seats
-        .reduce((prev, curr) => prev.concat(curr)); // flattening the 2d to 1d array
+        // filtering the selected seats
+        .map((m) => m.map.filter((s) => s.status === SeatStatus.SELECTED))
+        // flattening the 2d to 1d array
+        .reduce((prev, curr) => prev.concat(curr))
+        // need just the seat numbers
+        .map((s) => s.seatNumber);
       return seats;
     }
   }, [context?.seatMap]);
 
+  // add / remove passenger on seat selection / de-selection
+  useEffect(() => {
+    updateDetails((prevDetails: BookingDetails) => {
+      const newDetails: BookingDetails = {
+        bookingDate: prevDetails.bookingDate,
+        passengers: [...prevDetails.passengers],
+      };
+
+      const passengers = newDetails.passengers;
+
+      //seats to add
+      const newSelectedSeats = selectedSeats?.filter(
+        (s) => !newDetails.passengers.map((s) => s.seatNumber).includes(s),
+      );
+      if (newSelectedSeats?.length !== 0) {
+        newSelectedSeats?.forEach((s) =>
+          passengers.push({
+            seatNumber: s,
+            lastName: '',
+            firstName: '',
+            email: '',
+          }),
+        );
+        return newDetails;
+      }
+
+      //seats to remove
+      const deSelectedSeats = passengers.filter(
+        (s) => !selectedSeats?.includes(s.seatNumber),
+      );
+      if (deSelectedSeats?.length !== 0) {
+        deSelectedSeats?.forEach((s) => {
+          passengers.splice(
+            passengers.findIndex((p) => p.seatNumber === s.seatNumber),
+            1,
+          );
+        });
+      }
+      return newDetails;
+    });
+  }, [selectedSeats]);
+
   const isFormValid = useMemo(() => {
-    const isFormValid = true;
-    if (details.passengers.length === 0) {
+    let isFormValid = true;
+    if (passengers.length === 0) {
       return false;
     }
-    details.passengers.forEach((p) => {
+    passengers.forEach((p) => {
       if (
         p.firstName.trim().length === 0 ||
         p.lastName.trim().length === 0 ||
         p.email.trim().length === 0
       ) {
-        return false;
+        isFormValid = false;
       }
     });
     return isFormValid;
-  }, [details.passengers]);
-
-  useEffect(() => {
-    console.log('Is form valid', isFormValid);
-  }, [isFormValid]);
+  }, [passengers]);
 
   return (
     <div className={style.bookingFormLayout}>
       <h4 className={style.formTitle}>Booking Details</h4>
-      <div className={style.rows}>
-        {selectedSeats &&
-          selectedSeats.map((s) => (
-            <PassengerDetails
-              seatNumber={s.seatNumber}
-              key={s.seatNumber}
-              updateSeatDetails={updateBookingDetails}
-            />
-          ))}
+      <form className={style.rows}>
+        {passengers.map((s) => (
+          <PassengerDetails
+            seatNumber={s.seatNumber}
+            key={s.seatNumber}
+            updateSeatDetails={updateBookingDetails}
+          />
+        ))}
         <div className={style.actionRow}>
           <button
             className={style.btn}
@@ -160,7 +175,7 @@ export const BookingForm: React.FC = () => {
             Save
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
