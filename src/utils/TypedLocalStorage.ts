be391c716093a182b0bb4042a27fbac4d5707cde
Neil Doshi <mail.neildoshi@gmail.com>
2024-01-15 11:34:04 +0530

    seat save implemented successfully

---
+++ src/utils/TypedLocalStorage.ts
@@ -0,0 +1,28 @@
+export type Bookings = {
+  travelDate: Date;
+  seatNumber: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+};
+
+interface LocalStorageSchema {
+  bookings: {
+    bookings: Bookings[];
+  };
+}
+
+export function getLocalStorageItem<T extends keyof LocalStorageSchema>(
+  key: T,
+): LocalStorageSchema[T] {
+  return JSON.parse(
+    localStorage.getItem(key) ?? `{"error": "Object not found"}`,
+  );
+}
+
+export function setLocalStorageItem<T extends keyof LocalStorageSchema>(
+  key: T,
+  value: LocalStorageSchema[T],
+) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
